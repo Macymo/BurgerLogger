2fff4e12cc4dfc4f5704c009f06a321dedd0be77
Macy <59153195+Macymo@users.noreply.github.com>
2020-06-09 23:48:45 -0400

    initial commit, setup file structure

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Green Chile Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Salmon Burger');
