cb9f2123668973bc7a3b72ca71df4bada8abbb79
Macy <59153195+Macymo@users.noreply.github.com>
2020-06-10 21:22:07 -0400

    updated files

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-require('dotenv').config()
+require('dotenv').config();
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: process.env.myPassword,
+  password: process.env.MY_PASSWORD,
   database: "burgers_db"
 });
 
